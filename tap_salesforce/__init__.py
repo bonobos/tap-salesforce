40b9a54e2f309b82a353e78aecaf7dcb6e104acf
Aaron Pugliese <aaron.pugliese@bytecode.io>
2020-05-11 13:34:36 -0400

    added support for catalog argument

--- tap_salesforce/__init__.py
+++ tap_salesforce/__init__.py
@@ -10,6 +10,7 @@ from tap_salesforce.salesforce import Salesforce
 from tap_salesforce.salesforce.bulk import Bulk
 from tap_salesforce.salesforce.exceptions import (
     TapSalesforceException, TapSalesforceQuotaExceededException, TapSalesforceBulkAPIDisabledException)
+import argparse
 
 LOGGER = singer.get_logger()
 
@@ -362,8 +363,73 @@ def do_sync(sf, catalog, state):
     singer.write_state(state)
     LOGGER.info("Finished sync")
 
+def load_json(path):
+    with open(path) as fil:
+        return json.load(fil)
+
+def check_config(config, required_keys):
+    missing_keys = [key for key in required_keys if key not in config]
+    if missing_keys:
+        raise Exception("Config is missing required keys: {}".format(missing_keys))
+
+def parse_args(required_config_keys):
+    '''Parse standard command-line args.
+
+    Parses the command-line arguments mentioned in the SPEC and the
+    BEST_PRACTICES documents:
+
+    -c,--config     Config file
+    -s,--state      State file
+    -d,--discover   Run in discover mode
+    -p,--properties Properties file: DEPRECATED, please use --catalog instead
+    --catalog       Catalog file
+
+    Returns the parsed args object from argparse. For each argument that
+    point to JSON files (config, state, properties), we will automatically
+    load and parse the JSON file.
+    '''
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-c', '--config',
+        help='Config file',
+        required=True)
+
+    parser.add_argument(
+        '-s', '--state',
+        help='State file')
+
+    parser.add_argument(
+        '-p', '--properties',
+        help='Property selections: DEPRECATED, Please use --catalog instead')
+
+    parser.add_argument(
+        '--catalog',
+        help='Catalog file')
+
+    parser.add_argument(
+        '-d', '--discover',
+        action='store_true',
+        help='Do schema discovery')
+
+    args = parser.parse_args()
+    if args.config:
+        args.config = load_json(args.config)
+    if args.state:
+        args.state = load_json(args.state)
+    else:
+        args.state = {}
+    if args.properties:
+        args.properties = load_json(args.properties)
+    if args.catalog:
+        args.catalog = load_json(args.catalog)
+
+    check_config(args.config, required_config_keys)
+
+    return args    
+
 def main_impl():
-    args = singer_utils.parse_args(REQUIRED_CONFIG_KEYS)
+    args = parse_args(REQUIRED_CONFIG_KEYS)
     CONFIG.update(args.config)
 
     sf = None
@@ -382,8 +448,8 @@ def main_impl():
 
         if args.discover:
             do_discover(sf)
-        elif args.properties:
-            catalog = args.properties
+        elif args.catalog:
+            catalog = args.catalog
             state = build_state(args.state, catalog)
             do_sync(sf, catalog, state)
     finally:
